67aec34212d9a6fdd45f2e615972984a977f2ad9
Sergius Nyah <sergiusnyah@gmail.com>
2024-11-02 17:33:34 +0100

    Capture and process video frames

--- src/gui.py
+++ src/gui.py
@@ -46,3 +46,40 @@ last_frame1 = np.zeros((480, 640, 3), dtype = np.uint8) # initializing the frame
 global cap1 # video capture
 show_text = [0]  # text to show on screen
 
+
+'''
+Here, we capture and process Video Frames
+'''
+def show_vid():
+    cap1 = cv2.VideoCapture(0) # capturing video from the camera
+    if not cap1.isOpened(): # if the camera is not opened
+        print('Error! Check your camera')
+    flag1, frame1 = cap1.read() # reading the frame
+    frame1 = cv2.resize(frame1, (600, 500))  # resizing the frame
+
+    bounding_box = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+    gray_frame = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY) # converting the frame to grayscale
+    num_faces = bounding_box.detectMultiScale(gray_frame, scaleFactor = 1.3, minNeighbors = 5) # detecting faces in the frame
+
+    for (x, y, w, h) in num_faces: # for each face detected
+        cv2.rectangle(frame1, (x, y-50), (x+w, y+h+10), (255, 0, 0), 2) # draw a rectangle around the face
+        roi_gray_frame = gray_frame[y:y +h, x:x + w] # region of interest in the frame
+        cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray_frame, (48, 48)), -1), 0) # crop image
+        prediction = emotion_model.predict(cropped_img)
+
+        max_index = int(np.argmax(prediction)) # get the index of the emotion with the highest probability
+        show_text[0] = max_index # set the text to show on screen
+
+    if flag1 is None: # if the frame is not captured
+        print('Major Error!')
+    elif flag1:
+        global last_frame1
+        last_frame1 = frame1.copy()
+        pic = cv2.cvtColor(last_frame1, cv2.COLOR_BGR2RGB) # convert the frame to RGB
+        img = Image.fromarray(pic) # convert the frame to an image
+        imgtk = ImageTk.PhotoImage(image = img) # convert the image to a Tkinter image
+        lmain.imgtk = imgtk # set the image to the Tkinter image
+        lmain.configure(image = imgtk) # configure the image
+        lmain.after(10, show_vid) # call the function after 10ms
+    if cv2.waitKey(1) & 0xFF == ord('q'): # if 'q' (FOr Quit) is pressed
+        exit()
