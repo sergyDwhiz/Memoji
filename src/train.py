17b42a227619bdbe7034306fa87b6797dff174b2
Sergius Nyah <sergiusnyah@gmail.com>
2024-11-05 03:22:02 +0100

    Prepare train data

---
+++ src/train.py
@@ -0,0 +1,29 @@
+# Import Train Modules.
+import numpy as np
+import cv2
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D
+from keras.optimizers import Adam
+from keras.layers import MaxPooling2D
+from keras.preprocessing.image import ImageDataGenerator
+
+# Data preparation and preprocessing
+train_dir = 'data/train'
+val_dir = 'data/test'
+train_datagen = ImageDataGenerator(rescale=1./255)
+val_datagen = ImageDataGenerator(rescale=1./255)
+
+train_generator = train_datagen.flow_from_directory(
+        train_dir,
+        target_size=(48,48),
+        batch_size=64,
+        color_mode="grayscale",
+        class_mode='categorical')
+
+validation_generator = val_datagen.flow_from_directory(
+        val_dir,
+        target_size=(48,48),
+        batch_size=64,
+        color_mode="grayscale",
+        class_mode='categorical')
