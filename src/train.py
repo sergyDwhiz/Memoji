e5aca48b236bf5431e091ba16798501c230e7d85
Sergius Nyah <sergiusnyah@gmail.com>
2024-11-05 03:32:16 +0100

    Add emotion detection

--- src/train.py
+++ src/train.py
@@ -64,3 +64,28 @@ emotion_model_info = emotion_model.fit_generator(
         validation_data=validation_generator,
         validation_steps=7178 // 64)
 emotion_model.save_weights('emotion_model.h5')
+
+# Real time emotion detection
+cap = cv2.VideoCapture(0)
+while True: # loop to capture video frames
+    ret, frame = cap.read() # read the frame
+    if not ret: # if the frame is not read
+        break
+    bounding_box = cv2.CascadeClassifier('/') #
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    num_faces = bounding_box.detectMultiScale(gray_frame, scaleFactor=1.3, minNeighbors=5)
+
+    for (x, y, w, h) in num_faces: # for each face detected
+        cv2.rectangle(frame, (x, y-50), (x+w, y+h+10), (255, 0, 0), 2) # draw a rectangle around the face
+        roi_gray_frame = gray_frame[y:y + h, x:x + w] # region of interest in the frame
+        cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray_frame, (48, 48)), -1), 0) # crop image
+        emotion_prediction = emotion_model.predict(cropped_img) # predict the emotion
+        maxindex = int(np.argmax(emotion_prediction)) # get the index of the emotion with the highest probability
+        cv2.putText(frame, emotion_dict[maxindex], (x+20, y-60), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+
+    cv2.imshow('Video', cv2.resize(frame, (1200, 860), interpolation=cv2.INTER_CUBIC)) # show the video frame
+    if cv2.waitKey(1) & 0xFF == ord('q'): # if 'q' is pressed, quit. Else, continue
+        break
+
+cap.release()
+cv2.destroyAllWindows()
