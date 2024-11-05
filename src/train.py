7bedb637806daaefc5d4c4749cea983b2b3e9615
Sergius Nyah <sergiusnyah@gmail.com>
2024-11-05 03:26:02 +0100

    Build detection model

--- src/train.py
+++ src/train.py
@@ -9,7 +9,8 @@ from keras.layers import MaxPooling2D
 from keras.preprocessing.image import ImageDataGenerator
 
 # Data preparation and preprocessing
-train_dir = 'data/train'
+
+train_dir = 'data/train' # Path to the training and test data
 val_dir = 'data/test'
 train_datagen = ImageDataGenerator(rescale=1./255)
 val_datagen = ImageDataGenerator(rescale=1./255)
@@ -18,8 +19,8 @@ train_generator = train_datagen.flow_from_directory(
         train_dir,
         target_size=(48,48),
         batch_size=64,
-        color_mode="grayscale",
-        class_mode='categorical')
+        color_mode="grayscale", # grayscale images for faster processing and less memory usage
+        class_mode='categorical') # categorical labels for emotions
 
 validation_generator = val_datagen.flow_from_directory(
         val_dir,
@@ -27,3 +28,23 @@ validation_generator = val_datagen.flow_from_directory(
         batch_size=64,
         color_mode="grayscale",
         class_mode='categorical')
+
+# Build the CNN model (Detection Model)
+
+emotion_model = Sequential()
+
+emotion_model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(48,48,1)))
+emotion_model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+emotion_model.add(MaxPooling2D(pool_size=(2, 2)))
+emotion_model.add(Dropout(0.25))
+
+emotion_model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+emotion_model.add(MaxPooling2D(pool_size=(2, 2)))
+emotion_model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+emotion_model.add(MaxPooling2D(pool_size=(2, 2)))
+emotion_model.add(Dropout(0.25))
+
+emotion_model.add(Flatten())
+emotion_model.add(Dense(1024, activation='relu'))
+emotion_model.add(Dropout(0.5))
+emotion_model.add(Dense(7, activation='softmax'))
